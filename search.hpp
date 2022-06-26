74114853ecc3a2201f4d442bd4296b11a635385a
Torbjörn Rathsman <milasudril@gmail.com>
2022-06-26 12:27:25 +0200

    More work on search algo

--- search.hpp
+++ search.hpp
@@ -7,32 +7,60 @@
 #include <cmath>
 #include <numbers>
 #include <array>
+#include <algorithm>
 
 namespace cheapest_route
 {
 	using vec2i_t [[gnu::vector_size(16)]] = int64_t;
 	using vec2f_t [[gnu::vector_size(16)]] = double;
 
-	struct route_node
+	template<class T, auto tag>
+	struct tagged_value
 	{
-		vec2i_t loc;
-		vec2i_t from_loc;
+	public:
+		explicit tagged_value(T value):m_value{value}{}
+		T value() const { return m_value; }
+
+	private:
+		T m_value;
+	}
+
+	template<class T>
+	using from = tagged_value<T, 0>;
+
+	template<class T>
+	using to = tagged_value<T, 1>;
+
+	temlate<auto tag>
+	inline tagged_value<T, tag> operator+(tagged_value<T, tag> a, T b)
+	{
+		return tagged_value<T, tag>{a.value() + b};
+	}
+
+	struct pending_route_node
+	{
+		to<vec2i_t> loc;
 		double total_cost;
 	};
 
-	bool is_cheaper(route_node const& a, route_node const& b)
+	bool is_cheaper(pending_route_node const& a, pending_route_node const& b)
 	{
 		return a.total_cost < b.total_cost;
 	}
 
-	using path = std::vector<route_node>;
+	struct route_node
+	{
+		from<vec2i_t> loc;
+		double total_cost = std::numeric_limits<double>::infinity();
+	};
 
 	constexpr auto scale_factor = 16;
 
-	constexpr auto scale_by_factor(vec2i_t x)
+	template<auto tag>
+	constexpr auto scale_by_factor(tagged_value<vec2i_t, tag> x)
 	{
-		return static_cast<double>(scale_factor)
-			*vec2f_t{static_cast<double>(x[0]), static_cast<double>(x[1])};
+		return tagged_value<vec2f_t, tag>
+			{static_cast<double>(scale_factor)*vec2f_t{static_cast<double>(x[0]), static_cast<double>(x[1])}};
 	}
 
 	constexpr auto gen_neigbour_offset_table()
@@ -51,39 +79,48 @@ namespace cheapest_route
 	constexpr auto neigbour_offsets = gen_neigbour_offset_table();
 
 	template<class CostFunction, class ... Args>
-	path search(vec2i_t origin, CostFunction&& f, Args&& ... args)
+	auto search(from origin, CostFunction&& f, Args&& ... args)
 	{
-		auto cmp = [](route_node const& a, route_node const& b)
+		auto cmp = [](pending_route_node const& a, pending_route_node const& b)
 		{ return is_cheaper(a, b); }
 
-		std::priority_queue<route_node, decltype(cmp)> nodes_to_visit;
-		nodes_to_visit.push(route_node{scale_factor*origin, vec2i_t{-1, -1}, 0.0});
+		std::priority_queue<pending_route_node, decltype(cmp)> nodes_to_visit;
+		nodes_to_visit.push(pending_route_node{scale_factor*origin, 0.0});
 
-		auto loc_cmp=[](vec2i_t a, vec2i_t b)
-		{ return (a[0] == b[0]) ? a[1] < b[1] : a[0] < b[0]; }
+		auto loc_cmp=[](to p1, to p2) {
+			a = p1.value();
+			b = p2.value();
+			return (a[0] == b[0]) ? a[1] < b[1] : a[0] < b[0];
+		};
 
-		std::set<vec2i_t, loc_cmp> visited;
-		std::map<vec2f_t, double, loc_cmp> cost;
-		path ret;
+		std::map<to, std::pair<route_node, bool>, loc_cmp> cost_table;
 		while(!nodes_to_visit.empty())
 		{
 			auto current = nodes_to_visit.front();
 			nodes_to_visit.pop();
-			visited.insert(current.loc);
-			ret.push_back(current);
-			std::ranges::for_each(neigbour_offsets, [&visited, current](vec2i_t item) {
-				auto const next_loc = item + current.loc;
-				if(!visited.contains(next_node))
-				{
-					auto const new_cost = current.total_cost + f(scale_by_factor(current.loc), scale_by_factor(next_loc));
-					static_assert(std::is_same_v<std::decltype(cost), double>);
-					if(new_cost < cost[next_loc])
-					{ cost[next_loc] = new_cost; }
+			auto& cost_item = cost_table[current.loc];
+			cost_item.second = true;
 
-					nodes_to_visit.push(route_node{next_loc, current.loc, new_cost});
+			std::ranges::for_each(neigbour_offsets, [&visited, current](vec2i_t item) {
+ 				auto const next_loc = current.loc + item;
+				auto& new_cost_item = visited[next_loc];
+				if(!new_cost_item.second)
+				{
+					auto const new_cost = current.total_cost
+						+ f(scale_by_factor(from{current.loc.value()}), scale_by_factor(next_loc));
+					static_assert(std::is_same_v<std::decltype(cost), double>);
+					if(new_cost < new_cost_item.second.cost)
+					{
+						new_cost_item.first.cost = new_cost;
+						bew:cost_item.from = current.loc;
+					}
+
+					nodes_to_visit.push(pending_route_node{current.loc, new_cost});
 				}
 			});
 		}
+
+		return visited;
 	}
 }
 
