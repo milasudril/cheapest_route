47ba52011bbb92917761f8b69659c779948d3ef4
Torbjörn Rathsman <milasudril@gmail.com>
2022-06-27 20:59:01 +0200

    Add functions to vec class

--- vec.hpp
+++ vec.hpp
@@ -18,43 +18,105 @@ namespace cheapest_route
 	class vec
 	{
 	public:
+        vec() = default;
+
+        constexpr explicit vec(vec_t<T, N> val): m_value{val}{}
+
 		template<class ... Args>
-		explicit vec(T first, Args ... args):m_value{first, args...}{}
+        requires(sizeof...(Args) == N - 1)
+		constexpr explicit vec(T first, Args ... args):m_value{first, args...}{}
 
-		auto value() const { return m_value; }
+		constexpr auto value() const { return m_value; }
 
-		vec& operator+=(vec other)
+		constexpr vec& operator+=(vec other)
 		{
 			m_value += other.value();
 			return *this;
 		}
 
-		vec& operator-=(vec other)
+		constexpr vec& operator-=(vec other)
 		{
 			m_value -= other.value();
 			return *this;
 		}
 
-		vec& operator*=(T scalar)
+		constexpr vec& operator*=(T scalar)
 		{
 			m_value*=scalar;
 			return *this;
 		}
 
 		template<class U, auto other>
-		operator vec<U, N, other>() const
+		constexpr operator vec<U, N, other>() const
 		{
-			vec<U, N, other> ret{};
-			for(size_t k = 0; k != 0; ++k)
+			vec_t<U, N> ret;
+			for(size_t k = 0; k != N; ++k)
 			{
 				ret[k] = static_cast<U>(m_value[k]);
 			}
-			return ret;
+			return vec<U, N, other>{ret};
 		}
+
+        constexpr T operator[](size_t k) const { return m_value[k]; }
 
 	private:
 		vec_t<T, N> m_value;
 	};
+
+	template<class T, size_t N, auto tag>
+	requires std::is_floating_point_v<T>
+	auto operator/(vec<T, N, tag> val, T scalar)
+	{
+		return vec<T, N, tag>{val.value()/scalar};
+	}
+
+	template<class T, size_t N, auto tag>
+	auto operator*(T scalar, vec<T, N, tag> val)
+	{
+		val*=scalar;
+		return val;
+	}
+
+	template<class T, size_t N, auto tag>
+	auto operator*(vec<T, N, tag> val, T scalar)
+	{
+		val*=scalar;
+		return val;
+	}
+
+	template<class T, size_t N, auto tag>
+	auto operator+(vec<T, N, tag> a, vec<T, N, tag> b)
+	{
+		a+=b;
+		return a;
+	}
+
+	template<class T, size_t N, auto tag>
+	auto operator-(vec<T, N, tag> a, vec<T, N, tag> b)
+	{
+		a-=b;
+		return a;
+	}
+
+	template<class T, size_t N, auto tag>
+	auto dot(vec<T, N, tag> a, vec<T, N, tag> b)
+	{
+		T sum{};
+		a*=b;
+		for(size_t k = 0; k!= N; ++k)
+		{
+			sum += a[k];
+		}
+		return sum;
+	}
+
+	template<class T, size_t N, auto tag>
+	auto length_squared(vec<T, N, tag> a)
+	{
+		return dot(a ,a);
+	}
+
+
 }
 
 #endif
