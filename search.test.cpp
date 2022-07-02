9f7cc4be4d200857908daa037d80a25a24ee1722
Torbjörn Rathsman <milasudril@gmail.com>
2022-07-02 17:55:37 +0200

    Cleanup testcase

--- search.test.cpp
+++ search.test.cpp
@@ -1,9 +1,6 @@
 //@	{"target":{"name":"search.test"}}
 
 #include "./search.hpp"
-#include "./rectangle.hpp"
-
-#include <chrono>
 
 int main()
 {
@@ -12,16 +9,9 @@ int main()
 		cheapest_route::make_interval<cheapest_route::boundary_type::inclusive,
 			cheapest_route::boundary_type::exclusive>(0l, size);
 	auto const rect = cheapest_route::rectangle{valid_range, valid_range}.dimensions();
-#if 1
-	auto result1 = search(cheapest_route::from<int64_t>{size - 1, 2*size/3},
-		cheapest_route::to<int64_t>{0, size/3}, rect);
-#endif
-#if 0
-	auto result2 = search(cheapest_route::from<int64_t>{size - 1, 2*size/3},
-		cheapest_route::to<int64_t>{0, size/3}, cost_function);
-#endif
 
-//	printf("%.8e\n", std::chrono::duration<double>{std::chrono::steady_clock::now() - t0}.count());
-//	follow_path(result1, cheapest_route::to<int64_t>{size - 1, 2*size/3});
-	follow_path(result1, cheapest_route::to<int64_t>{0, size/3});
+	auto result = search(cheapest_route::from<int64_t>{size - 1, 2*size/3},
+		cheapest_route::to<int64_t>{0, size/3}, rect);
+
+	follow_path(result, cheapest_route::to<int64_t>{0, size/3});
 }
